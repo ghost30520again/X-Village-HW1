e6e33eb94081f80cd1abb6e6b9a7e31288bcf7eb
CCMing <sdf7862@gmail.com>
2018-07-20 20:25:51 +0800

    version 2

--- matrix.py
+++ matrix.py
@@ -1,9 +1,10 @@
 import random
-from copy import deepcopy
 
 class Matrix:
+     
 
     def __init__(self, nrows, ncols):
+        
         self.matrixa = []
         self.nrows = nrows
         self.ncols = ncols
@@ -13,9 +14,9 @@ class Matrix:
     
             for j in range(1,self.ncols+1):       #column
                 self.matrixa[i-1].append(random.randint(0,9))
-        print(self.matrixa)
-
-    def add(self, m):
+        
+    def add(self, m): 
+        
         self.nrows
         self.ncols 
         c = Matrix(self.nrows, self.ncols)
@@ -23,8 +24,9 @@ class Matrix:
             for j in range(0,self.ncols):                                      
                 c.matrixa[i][j]= self.matrixa[i][j]+m.matrixa[i][j]
         return c
-
-    def sub(self, m):
+    
+    def sub(self, m): 
+        
         self.nrows
         self.ncols 
         c = Matrix(self.nrows, self.ncols)
@@ -32,8 +34,9 @@ class Matrix:
             for j in range(0,self.ncols):                                      
                 c.matrixa[i][j]= self.matrixa[i][j]-m.matrixa[i][j]
         return c
-
-    def mul(self, m):
+    
+    def mul(self, m): 
+        
         self.nrows
         self.ncols 
         c = Matrix(self.nrows,m.ncols)
@@ -47,34 +50,49 @@ class Matrix:
                 for k in range(0,self.ncols):                                             
                     c.matrixa[i][j] += self.matrixa[i][k]*m.matrixa[k][j]                             
         return c
-
+    
     def transpose(self):
-        self.nrows
-        self.ncols
         c = Matrix(self.nrows, self.ncols)
         
         for i in range(0,self.nrows):   
             for j in range(0,self.ncols):                                      
                 c.matrixa[i][j]= 0
         
-        c = [[self.nrows[i] for self.nrows in self.matrixa] for i in range(0,self.nrows)]
-        
+        c.matrixa = [[self.nrows[i] for self.nrows in self.matrixa] for i in range(0,self.nrows)]
+            
         return c
-    
+        
     def display(self):
+            
         for i in range(0,self.nrows):
             for j in range(0,self.ncols):
                 print(self.matrixa[i][j],end="  ")              
-            print(" ") 
+            print(" ")                    
+            
+            
+print("產生矩陣A  3*3")
+print("===============")
+A=Matrix(3,3)
+print(A.display())
+print("===============")
+print("產生矩陣B  3*3")
+B=Matrix(3,3)
+print(B.display())
+print("===============")
 
-A=Matrix(3,2)
-B=Matrix(2,4)
-
-#A.add(B).display()             #輸出結果的第三行是用來存儲結果的矩陣
-#A.sub(B).display()
-A.mul(B).display()              #矩陣3*2乘上矩陣2*4=矩陣3*4   
-#A.transpose()
-
-
-
-
+print("======A+B======")
+C=A.add(B)
+print(C.display())
+print("======A-B======")
+D=A.sub(B)
+print(D.display())
+print("======A*B======")
+E=A.mul(B)
+print(D.display())
+print("==Transpose A==")
+F=A.transpose()
+print(F.display())
+print("==Transpose B==")
+G=B.transpose()
+print(G.display())
+print("===============")
